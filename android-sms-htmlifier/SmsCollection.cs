ea7d14e04f568781fea004af90a34b8c771bee8e
James Robertson <jamestherobertson@gmail.com>
2012-10-26 17:39:52 +0100

    Remove reltime placeholder, we'll calculate/show this with JS

--- android-sms-htmlifier/SmsCollection.cs
+++ android-sms-htmlifier/SmsCollection.cs
@@ -1,4 +1,4 @@
-﻿/*****************************************************************************
+/*****************************************************************************
  * android-sms-htmlifier
  * Converts android sms xml backup file to useful html format.
  * Copyright James Robertson, Ted John 2012
@@ -90,7 +90,6 @@ namespace rd3korca.AndroidSmsHtmlifier
 				string newMessage = msgHtml;
 				string sender = Program.HtmlSpecialChars(sms.Sender);
 				newMessage = newMessage.Replace("{{TIME}}", Program.HtmlSpecialChars(sms.Timestamp.ToString()));
-				newMessage = newMessage.Replace("{{RELTIME}}", sms.Timestamp.ToFileTime().ToString());
 				newMessage = newMessage.Replace("{{SENDER}}", sender);
 				newMessage = newMessage.Replace("{{CONTENT}}", Program.HtmlSpecialChars(sms.Body));
 				sb.Append(newMessage);
