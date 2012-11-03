e521c9446479153bcfb0d655aa3dbe4d9740786d
Ted John <ted@brambles.org>
2012-11-03 18:46:25 +0000

    Add DateTime to unix timestamp extension method

--- android-sms-htmlifier/Program.cs
+++ android-sms-htmlifier/Program.cs
@@ -10,7 +10,7 @@ using System.Linq;
 
 namespace rd3korca.AndroidSmsHtmlifier
 {
-	class Program
+	static class Program
 	{
 		static void Main(string[] args)
 		{
@@ -26,16 +26,35 @@ namespace rd3korca.AndroidSmsHtmlifier
 			return text;
 		}
 
+		/// <summary>
+		/// Converts a unix time to a DateTime object.
+		/// </summary>
+		/// <param name="unixTime">Unix time to convert.</param>
+		/// <returns>a DateTime object.</returns>
 		public static DateTime FromUnixTime(string unixTime)
 		{
 			return FromUnixTime(Int64.Parse(unixTime));
 		}
 
+		/// <summary>
+		/// Converts a unix time to a DateTime object.
+		/// </summary>
+		/// <param name="unixTime">Unix time to convert.</param>
+		/// <returns>a DateTime object.</returns>
 		public static DateTime FromUnixTime(long unixTime)
 		{
 			var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 			return epoch.AddMilliseconds(unixTime);
 		}
 
+		/// <summary>
+		/// Converts a DateTime object to a unix timestamp.
+		/// </summary>
+		/// <param name="dt">DateTime object to convert to a unix timestamp.</param>
+		/// <returns>a long representing a unix timestamp.</returns>
+		public static long ToUnixTime(this DateTime dt)
+		{
+			return (long)(dt - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).ToLocalTime()).TotalMilliseconds;
+		}
 	}
 }
