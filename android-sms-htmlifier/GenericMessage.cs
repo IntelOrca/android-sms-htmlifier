b3f2ca1a015973e7f1182d4e3f03b079acefa0bc
Ted John <ted@brambles.org>
2012-11-04 22:43:17 +0000

    Add message interface and a generic message class

---
+++ android-sms-htmlifier/GenericMessage.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace rd3korca.AndroidSmsHtmlifier
+{
+	struct GenericMessage : IMessage
+	{
+		private DateTime mTimestamp;
+		private string mFromContact;
+		private string[] mToContacts;
+		private string mText;
+
+		public override string ToString()
+		{
+			return String.Format("{0}: {1}", mFromContact, mText);
+		}
+
+		public static GenericMessage[] FromXmlFile(string path)
+		{
+			List<GenericMessage> genericMessages = new List<GenericMessage>();
+
+			// Load xml document
+			XmlDocument doc = new XmlDocument();
+			doc.Load(path);
+
+			// Read each message element
+			foreach (XmlNode messageNode in doc.SelectNodes("Log/Message")) {
+				GenericMessage message = new GenericMessage();
+
+				message.Timestamp = DateTime.Parse(messageNode.Attributes["DateTime"].Value);
+				message.FromContact = messageNode.SelectSingleNode("From/User").Attributes["FriendlyName"].Value;
+
+				List<string> toContacts = new List<string>();
+				foreach (XmlNode userNode in messageNode.SelectNodes("To/User")) {
+					XmlAttribute attribute = userNode.Attributes["FriendlyName"];
+					if (attribute == null)
+						continue;
+					toContacts.Add(attribute.Value);
+				}
+				message.ToContacts = toContacts.ToArray();
+
+				message.Text = messageNode.SelectSingleNode("Text").InnerText;
+
+				genericMessages.Add(message);
+			}
+
+			return genericMessages.ToArray();
+		}
+
+		public DateTime Timestamp
+		{
+			get
+			{
+				return mTimestamp;
+			}
+			set
+			{
+				mTimestamp = value;
+			}
+		}
+
+		public string FromContact
+		{
+			get
+			{
+				return mFromContact;
+			}
+			set
+			{
+				mFromContact = value;
+			}
+		}
+
+		public string[] ToContacts
+		{
+			get
+			{
+				return mToContacts;
+			}
+			set
+			{
+				mToContacts = value;
+			}
+		}
+
+		public string Text
+		{
+			get
+			{
+				return mText;
+			}
+			set
+			{
+				mText = value;
+			}
+		}
+	}
+}
---
+++ android-sms-htmlifier/IMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace rd3korca.AndroidSmsHtmlifier
+{
+	/// <summary>
+	/// Interface to represent a single message from someone to someone.
+	/// </summary>
+	interface IMessage
+	{
+		DateTime Timestamp
+		{
+			get;
+		}
+
+		string FromContact
+		{
+			get;
+		}
+
+		string[] ToContacts
+		{
+			get;
+		}
+
+		string Text
+		{
+			get;
+		}
+	}
+}
